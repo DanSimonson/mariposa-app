f86b60f25fe70b4ef08fecd802144905559145d1
daniel simonson <dansimonson@mariposaweb.net>
2021-11-22 02:07:16 -0600

    add heroku

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import projectRouter from "./routers/projectRouter.js";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
+import path from "path";
 
 dotenv.config();
 connectDB();
@@ -13,6 +14,12 @@ app.use(express.urlencoded({ extended: true }));
 
 //routes
 app.use("/api/projects", projectRouter);
+
+const __dirname = path.resolve();
+// app.use(express.static(path.join(__dirname, "/frontend/build")));
+// app.get("*", (req, res) =>
+//   res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+// );
 
 const port = process.env.PORT || 5000;
 
