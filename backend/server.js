6702bacb1c589e3d62129f7c520631b972be021c
daniel simonson <dansimonson@mariposaweb.net>
2021-11-22 02:09:26 -0600

    publish to heroku

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/projects", projectRouter);
 
 const __dirname = path.resolve();
-// app.use(express.static(path.join(__dirname, "/frontend/build")));
-// app.get("*", (req, res) =>
-//   res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
-// );
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+);
 
 const port = process.env.PORT || 5000;
 
